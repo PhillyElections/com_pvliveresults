3db4ddc9ef0007cbe8637a373660c281481e1fc3
matt murphy <mattyhead@gmail.com>
2016-09-19 15:02:03 -0400

    woops.  misplaced ,

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -112,7 +112,7 @@ SET FOREIGN_KEY_CHECKS = 1;
 INSERT INTO `#__pv_live_vote_types` VALUES
 ('', 'ABSENTEE', 1, 1, NOW(), '0000-00-00 00:00:00'),
 ('', 'MACHINE', 2, 1, NOW(), '0000-00-00 00:00:00'),
-('', 'PROVISIONAL'3, , 1, NOW(), '0000-00-00 00:00:00');
+('', 'PROVISIONAL', 3, 1, NOW(), '0000-00-00 00:00:00');
 
 INSERT INTO `#__pv_live_parties` VALUES
 ('', '', 1, 1, NOW(), '0000-00-00 00:00:00'),
