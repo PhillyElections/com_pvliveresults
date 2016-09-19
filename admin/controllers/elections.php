310a8665ff8bdbe6eda3a8762082658093bdb4a0
matt murphy <mattyhead@gmail.com>
2016-09-19 13:40:13 -0400

    Let's load *everything* and poke at 'em before doing work

--- admin/controllers/elections.php
+++ admin/controllers/elections.php
@@ -11,7 +11,12 @@ class PvliveresultsControllerElections extends PvliveresultsController
     {
         // if 'raw' isn't explicit, set to 'html'
         $view = $this->getView('elections', JRequest::getWord('format', 'html'));
+        $view->setModel($this->getModel('candidate'), true);
         $view->setModel($this->getModel('election'), true);
+        $view->setModel($this->getModel('office'), true);
+        $view->setModel($this->getModel('party'), true);
+        $view->setModel($this->getModel('vote'), true);
+        $view->setModel($this->getModel('votetype'), true);
 
         $view->display();
     }
