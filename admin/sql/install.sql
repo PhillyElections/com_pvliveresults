cc9497693f56c918ba31db542b9e8067fcf56d9d
matt murphy <mattyhead@gmail.com>
2016-09-19 15:00:52 -0400

    putting back those delete queries for the dev cycle

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -1,6 +1,14 @@
 SET FOREIGN_KEY_CHECKS = 0;
 SELECT @@FOREIGN_KEY_CHECKS;
 
+DROP TABLE IF EXISTS `#__pv_live_candidates`;
+DROP TABLE IF EXISTS `#__pv_live_votes`;
+DROP TABLE IF EXISTS `#__pv_live_divisions`;
+DROP TABLE IF EXISTS `#__pv_live_election_years`;
+DROP TABLE IF EXISTS `#__pv_live_offices`;
+DROP TABLE IF EXISTS `#__pv_live_parties`;
+DROP TABLE IF EXISTS `#__pv_live_wards`;
+
 CREATE TABLE IF NOT EXISTS `#__pv_live_elections` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT
 , `name` varchar(100) NOT NULL
