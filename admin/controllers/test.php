4f31f4f5a745b7b672a5ce37f64b39ee721279b9
matt murphy <mattyhead@gmail.com>
2016-09-22 10:35:38 -0400

    adding in vote model

--- admin/controllers/test.php
+++ admin/controllers/test.php
@@ -14,8 +14,8 @@ class PvliveresultsControllerTest extends PvliveresultsController
         $view->setModel($this->getModel('candidate'), true);
 /*        $view->setModel($this->getModel('election'), true);
         $view->setModel($this->getModel('office'), true);
-        $view->setModel($this->getModel('party'), true);
-        $view->setModel($this->getModel('vote'), true);*/
+        $view->setModel($this->getModel('party'), true);*/
+        $view->setModel($this->getModel('vote'), true);
         $view->setModel($this->getModel('votetype'), true);
 
         $view->display();
