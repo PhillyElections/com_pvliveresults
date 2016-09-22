89120954aea0eb34025cfa069c47c3620b592221
matt murphy <mattyhead@gmail.com>
2016-09-22 10:34:35 -0400

    including votetype model

--- admin/controllers/test.php
+++ admin/controllers/test.php
@@ -15,8 +15,8 @@ class PvliveresultsControllerTest extends PvliveresultsController
 /*        $view->setModel($this->getModel('election'), true);
         $view->setModel($this->getModel('office'), true);
         $view->setModel($this->getModel('party'), true);
-        $view->setModel($this->getModel('vote'), true);
-        $view->setModel($this->getModel('votetype'), true);*/
+        $view->setModel($this->getModel('vote'), true);*/
+        $view->setModel($this->getModel('votetype'), true);
 
         $view->display();
     }
