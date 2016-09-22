d9e73db520cd5ab3d3eba7f4ab75c0f970342fe6
matt murphy <mattyhead@gmail.com>
2016-09-22 10:25:28 -0400

    further reduction of dumping, in order to isolate error producing file

--- admin/controllers/test.php
+++ admin/controllers/test.php
@@ -11,12 +11,12 @@ class PvliveresultsControllerTest extends PvliveresultsController
     {
         // if 'raw' isn't explicit, set to 'html'
         $view = $this->getView('test', 'html');
-        $view->setModel($this->getModel('candidate'), true);
+/*        $view->setModel($this->getModel('candidate'), true);
         $view->setModel($this->getModel('election'), true);
         $view->setModel($this->getModel('office'), true);
         $view->setModel($this->getModel('party'), true);
         $view->setModel($this->getModel('vote'), true);
-        $view->setModel($this->getModel('votetype'), true);
+        $view->setModel($this->getModel('votetype'), true);*/
 
         $view->display();
     }
