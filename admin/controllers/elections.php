1e097ea654f6bdd690cc5f2572b1209ea89bf20e
matt murphy <mattyhead@gmail.com>
2016-09-23 13:51:50 -0400

    Let's try getinstance

--- admin/controllers/elections.php
+++ admin/controllers/elections.php
@@ -25,7 +25,7 @@ class PvliveresultsControllerElections extends PvliveresultsController
         // if format isn't explicit, set to 'html'
         $view = $this->getView('elections', JRequest::getWord('format', 'html'));
         d('loaded view in display of elections controller');
-        $model = $this->getModel('elections');
+        $model = JFactory::getInstance('elections', 'model')
         d($model);
         $view->setModel($model, true);
         d('loaded model in display of elections controller');
