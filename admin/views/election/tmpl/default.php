a31757c8039ee726d4663b4f7717a67d774bd2fb
matt murphy <mattyhead@gmail.com>
2016-09-23 10:50:24 -0400

    Can't remember -- check for errors now

--- admin/views/election/tmpl/default.php
+++ admin/views/election/tmpl/default.php
@@ -1,6 +1,8 @@
 <?php 
 defined('_JEXEC') or die('Restricted access');
 jimport( 'joomla.html.html' );
+$election=$this->election;
+$offices=$this->offices;
 ?>
 <script>
 
@@ -10,36 +12,36 @@ jimport( 'joomla.html.html' );
     <table class="adminform">
         <tr>
             <td>Election Name</td>
-            <td><input type="text" name="election_name" value="<?=$this->election[1][0]->e_year; ?>"></td>
+            <td><input type="text" name="election_name" value="<?=$election->name; ?>"></td>
         </tr>
         <tr>
             <td>Deleted</td>
-            <td><input type="checkbox" name="published" <?= $this->election[1][0]->published ? "checked='checked'" :'' ?>></td>
+            <td><input type="checkbox" name="published" <?= $election->published ? "checked='checked'" :'' ?>></td>
         </tr>
         <tr>
             <td>Election Date</td>
             <td class="paramlist_value">
-                <?=JHTML::calendar( $this->election[1][0]->election_date, 'election_date', 'election_date', '%Y-%m-%d', 'class="inputbox" size="25" maxlength="19"' ) ; ?>
+                <?=JHTML::calendar( $election->date, 'date', 'date', '%Y-%m-%d', 'class="inputbox" size="25" maxlength="19"' ) ; ?>
             </td>
         </tr>
-        
+     *
     </table>
     <table class="adminlist">
     <thead>
         <tr>
             <th width="1">
-                <?=JText::_( 'NUM' ); ?>
+                <?=JText::_( 'ID' ); ?>
             </th>
             <th width="1">
-                <input type="checkbox" name="toggle" value="" onclick="checkAll(<?=count( $this->election[0] ); ?>);" />
+                <input type="checkbox" name="toggle" value="" onclick="checkAll(<?=count( $offices ); ?>);" />
             </th>
             <th width="0">
                 <?=JText::_( 'NAME' ); ?>
             </th>
             <th width="15">
-                <?=JText::_( 'PUBLISH' ); ?>
-            </th>            
-            <th width="20">
+                <?=JText::_( 'PUBLISHED' ); ?>
+            </th>     *
+            <th width="20">`
                 <?=JText::_( 'ORDER' ); ?>
             </th>
         </tr>
@@ -47,8 +49,8 @@ jimport( 'joomla.html.html' );
     <?php
     $k = 0;
     $election_year_id = 0;
-    for ($i=0, $n=count( $this->election[0] ); $i < $n; $i++) {
-        $row = &$this->election[0][$i];
+    for ($i=0, $n=count( $offices ); $i < $n; $i++) {
+        $row = &$offices[$i];
         $link = JRoute::_( 'index.php?option=com_pvliveresults&controller=election&task=edit&cid[]=' . $row->id );
         $election_year_id = $row->election_id;
         ?>
@@ -69,7 +71,7 @@ jimport( 'joomla.html.html' );
                 <?=JHTML::_('grid.published', $row, $i );?>
             </td>
             <td>
-                <input type="text" name="publish_order[<?=$row->id ;?>]" value="<?=$row->publish_order ?>" />
+                <input type="text" name="ordering[<?=$row->id ;?>]" value="<?=$row->ordering ?>" />
             </td>
         </tr>
         <?php
@@ -80,8 +82,8 @@ jimport( 'joomla.html.html' );
 </div>
 
 <input type="hidden" name="option" value="com_pvliveresults" />
-<input type="hidden" name="id" value="<?=$election_year_id; ?>" />
-<input type="hidden" name="task" value="save_step2" />
+<input type="hidden" name="id" value="<?=$election->id; ?>" />
+<input type="hidden" name="task" value="save" />
 <input type="hidden" name="boxchecked" value="0" />
 <input type="hidden" name="controller" value="election" />
 <?php echo JHTML::_('form.token');?>
