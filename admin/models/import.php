ee33b72194271d2c0c6c95abadc9abf6f3cec71a
matthew murphy <mattyhead@gmail.com>
2016-10-09 18:29:27 -0400

    stray ' in query

--- admin/models/import.php
+++ admin/models/import.php
@@ -81,7 +81,7 @@ class PvliveresultsModelImport extends PvliveresultsModel
     {
         $table = "#__pv_live_offices";
         $field = 'office';
-        $query = " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` ' from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )";
+        $query = " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )";
 
         return $this->_getList($query);
     }
