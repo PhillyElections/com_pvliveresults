e50f61d58c8ded01cfeffb46c0a3f7079be5c2aa
matthew murphy <mattyhead@gmail.com>
2016-10-09 18:12:23 -0400

    woops.  _getList() gets a query as a param

--- admin/models/import.php
+++ admin/models/import.php
@@ -67,9 +67,9 @@ class PvliveresultsModelImport extends PvliveresultsModel
     {
         $table = "#__pv_live_candidates";
         $field = 'candidate';
-        $this->_db->setQuery("SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` ' from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )");
+        $query = " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` ' from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )";
 
-        return $this->_getList();
+        return $this->_getList($query);
     }
 
     /**
@@ -81,9 +81,9 @@ class PvliveresultsModelImport extends PvliveresultsModel
     {
         $table = "#__pv_live_offices";
         $field = 'office';
-        $this->_db->setQuery("SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` ' from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )");
+        $query = " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` ' from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )";
 
-        return $this->_getList();
+        return $this->_getList($query);
     }
 
     /**
@@ -95,8 +95,8 @@ class PvliveresultsModelImport extends PvliveresultsModel
     {
         $table = "#__pv_live_parties";
         $field = 'party';
-        $this->_db->setQuery("SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` ' from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )");
+        $query = " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` ' from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )";
 
-        return $this->_getList();
+        return $this->_getList($query);
     }
 }
