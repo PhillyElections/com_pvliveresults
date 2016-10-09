23ded60b7728eb987f4103d97ad66c03b2e69b0d
matthew murphy <mattyhead@gmail.com>
2016-10-09 19:16:33 -0400

    explicitly set field names on the target table

--- admin/models/import.php
+++ admin/models/import.php
@@ -95,8 +95,8 @@ class PvliveresultsModelImport extends PvliveresultsModel
     {
         $table = "#__pv_live_parties";
         $field = 'party';
-        $this->_db->setQuery(" INSERT INTO " . $this->_db->nameQuote($table) . " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )");
-        d(" INSERT INTO " . $this->_db->nameQuote($table) . " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )");
+        $this->_db->setQuery(" INSERT INTO " . $this->_db->nameQuote($table) . " (`name`, `published`, `created`) SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " ) ");
+        d(" INSERT INTO " . $this->_db->nameQuote($table) . " (`name`, `published`, `created`) SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " ) ");
         $this->_db->query();
     }
 }
