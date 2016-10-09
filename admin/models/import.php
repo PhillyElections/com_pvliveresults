797f8a38cb7ae3a0ceaab8610b300cdbbcb1b63f
matthew murphy <mattyhead@gmail.com>
2016-10-09 18:36:51 -0400

    let's go live on parties!

--- admin/models/import.php
+++ admin/models/import.php
@@ -95,8 +95,8 @@ class PvliveresultsModelImport extends PvliveresultsModel
     {
         $table = "#__pv_live_parties";
         $field = 'party';
-        $query = " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )";
-        d($query);
-        return $this->_getList($query);
+        $this->_db->setQuery(" INSERT INTO " . $this->_db->nameQuote($table) . " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )");
+
+        $this->_db->query();
     }
 }
