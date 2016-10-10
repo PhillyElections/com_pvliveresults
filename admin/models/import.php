6599630119fb8b1d01bbd84fa6f058ac1fb26dca
matthew murphy <mattyhead@gmail.com>
2016-10-09 21:20:53 -0400

    denuding import fornow

--- admin/models/import.php
+++ admin/models/import.php
@@ -58,45 +58,4 @@ class PvliveresultsModelImport extends PvliveresultsModel
      */
     //public $_fk = '';
 
-    /**
-     * disinct array of candidates
-     *
-     * @return array()
-     */
-    public function addNewCandidates()
-    {
-        $table = "#__pv_live_candidates";
-        $field = 'candidate';
-        $query = " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )";
-
-        return $this->_getList($query);
-    }
-
-    /**
-     * disinct array of offices
-     *
-     * @return array()
-     */
-    public function addNewOffices()
-    {
-        $table = "#__pv_live_offices";
-        $field = 'office';
-        $query = " SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " )";
-
-        return $this->_getList($query);
-    }
-
-    /**
-     * disinct array of parties
-     *
-     * @return array()
-     */
-    public function addNewParties()
-    {
-        $table = "#__pv_live_parties";
-        $field = 'party';
-        $this->_db->setQuery(" INSERT INTO " . $this->_db->nameQuote($table) . " (`name`, `published`, `created`) SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " ) ");
-        d(" INSERT INTO " . $this->_db->nameQuote($table) . " (`name`, `published`, `created`) SELECT DISTINCT " . $this->_db->nameQuote($field) . " as `name`, 1 as `published`, '" . $this->getNow() . "' as `created` from " . $this->_db->nameQuote($this->_table) . " WHERE " . $this->_db->nameQuote($field) . " NOT IN (SELECT `name` FROM " . $this->_db->nameQuote($table) . " ) ");
-        $this->_db->query();
-    }
 }
